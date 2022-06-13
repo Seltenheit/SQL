b8f9814145ba4f6a309a0f008ba35e542fd66249
Seltenheit <104844876+Seltenheit@users.noreply.github.com>
2022-06-13 19:13:18 +0300

    HAVING, GROUP BY, multiple data tables

---
+++ SQLQuery04.sql
@@ -0,0 +1,5 @@
+﻿/* Goal: Find products which order value is more than 50 */
+SELECT DISTINCT Products.ProductID as 'ID', Products.ProductName as 'Name',
+Products.UnitPrice as 'Unit price', [Order Details].UnitPrice 'Order price'
+FROM Products, [Order Details]
+WHERE [Order Details].UnitPrice > 50 AND [Order Details].ProductID = Products.ProductID; 
---
+++ SQLQuery05.sql
@@ -0,0 +1,18 @@
+﻿/* Filter employees by territories 
+
+
+SELECT EmployeeID as id, COUNT(*) as territory
+FROM EmployeeTerritories
+GROUP BY EmployeeID
+HAVING COUNT(*) > 1
+
+Finding max territory one employee takes */
+
+SELECT MAX(tmp.territory) as 'Max territory'
+FROM
+(
+SELECT EmployeeID as id, COUNT(*) as territory
+FROM EmployeeTerritories
+GROUP BY EmployeeID
+HAVING COUNT(*) > 1
+) tmp
